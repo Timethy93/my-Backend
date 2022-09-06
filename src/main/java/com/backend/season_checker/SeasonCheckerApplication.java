3408ae10184b149cd3965e361b9a7d07f880adf3
ToddxD <toad915@gmail.com>
2022-09-06 10:08:56 +0200

    refactoring

--- src/main/java/com/backend/season_checker/SeasonCheckerApplication.java
+++ src/main/java/com/backend/season_checker/SeasonCheckerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SeasonCheckerApplication {
 
 	public static void main(String[] args) {
-		System.out.println();
 		SpringApplication.run(SeasonCheckerApplication.class, args);
 	}
 
